f382c9b2bfa5a23f2ce516c9c4e03072e5a953cb
pingnak <pingnak@gmail.com>
2015-03-29 15:06:47 -0700

    Downloaded
    Got all the bits downloaded

--- download.sh
+++ download.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-# For windows users, well, sorry.  I'm not going to write a DOS/cmd batch for you, right now. 
-
-
+#
+# Implied requirement: Java build environment (JDK)
+#
+# Apache Ant - build scripting
+# http://ant.apache.org/
+#
 # COCOS 2D Requirements
 # http://www.cocos2d-x.org/download
-
+#
+# Android requirements for Cocos2D
+# https://developer.android.com/sdk/installing/index.html
+# https://developer.android.com/tools/sdk/ndk/index.html
+#
+# iOS requirements for Cocos2D
+# OS X, running on a Mac
+# XCODE, from the 'App Store'
+# All of the relevent XCODE bits, downloaded from XCODE
+#
 # Adobe AIR
 # https://get.adobe.com/air/
 # http://www.adobe.com/devnet/air/air-sdk-download.html
-# http://ant.apache.org/
-#
-
+# 
 #
 # Fix AIR SDK fdb, so it 'works'.  Once you have it in your path, type 'fdb'.  It complains about wanting a 32 bit java runtime.  
 # Edit the 'fdb' script in the bin folder.  Replace the 'java' invocation with a hard-coded path to java
@@ -22,6 +32,16 @@
 # https://support.apple.com/kb/DL1572?locale=en_US
 #
 
+#
+# For windows users, this will be a separate 'download.bat' file.
+# One giant pile of testing, at a time...
+#
+# Windows 7
+# Visual Studio 2012+
+# Python 2.7
+#
+
+
 # Set BASH options 
 set -o errexit	# Stop running the script if an error occurs
 set -o nounset	# Stop running the script if a variable isn't set
@@ -29,53 +49,69 @@ set -o verbose	# Echo every command
 
 echo Downloading Pieces
 
-downloads=./dependencies
+downloads=./depends
 wget_flags="--continue --directory-prefix=$downloads"
 
+#
 # Get Apache ANT
+#
 wget $wget_flags https://www.apache.org/dist/ant/binaries/apache-ant-1.9.4-bin.zip
+unzip -d $downloads "$downloads/apache-ant-1.9.4-bin.zip"
 
-# Get Cocos2D-js
-wget $wget_flags http://www.cocos2d-x.org/filedown/cocos2d-js-v3.3.zip
+#
+# Get Adobe AIR SDK
+#
 
-pushd $downloads
-unzip -u -d ant "apache-ant-1.9.4-bin.zip"
-unzip -u -d cocos2d-js "cocos2d-js-v3.3.zip"
-popd
-
+# Get the rest of the AIR SDK requirements...
 if [ `uname` = Darwin ] ; then
     # Adobe AIR SDK
     wget $wget_flags http://airdownload.adobe.com/air/mac/download/latest/AIRSDK_Compiler.tbz2
-    # OpenFLEX
-#   wget $wget_flags http://www.apache.org/dyn/closer.cgi?path=/flex/installer/3.1/binaries/apache-flex-sdk-installer-3.1.0-bin.dmg
-    # Get various OS X Android parts
-    wget $wget_flags http://dl.google.com/android/android-sdk_r24.1.2-macosx.zip
-    wget $wget_flags http://dl.google.com/android/ndk/android-ndk-r10d-darwin-x86_64.bin
-
-pushd $downloads
-    tar jvxfk -C AIR "AIRSDK_Compiler.tbz2"
-    unzip -u -d android-sdk "android-sdk_r24.1.2-macosx.zip"
-
-    # Evil, self-extracting installer.    
-    chmod +x "android-ndk-r10d-darwin-x86_64.bin"
-    ./android-ndk-r10d-darwin-x86_64.bin
-popd
-        
+    mkdir -p "$downloads/AIRSDK"
+    tar jxfk "$downloads/AIRSDK_Compiler.tbz2" -C "$downloads/AIRSDK"
 else
-    # If not running in OS X, assume Linux
-    wget $wget_flags http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin
-    wget $wget_flags http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz
     # OpenFLEX
     wget $wget_flags http://www.apache.org/dyn/closer.cgi?path=/flex/installer/3.1/apache-flex-sdk-installer-3.1.0-src.tar.gz
+    mkdir -p "$downloads/FlexSDK"
+    tar xzf "$downloads/apache-flex-sdk-installer-3.1.0-src.tar.gz" -C "$downloads/FlexSDK"
+fi
 
-pushd $downloads
-    tar xvzfk -C flex "apache-flex-sdk-installer-3.1.0-src.tar.gz"
-    tar xvzfk -C 'android-sdk' "android-sdk_r24.1.2-linux.tgz"
+#
+# Get Cocos2D and other bits and pieces
+#
 
-    # Evil, self-extracting installer.    
+# Get Cocos2D-js
+wget $wget_flags http://www.cocos2d-x.org/filedown/cocos2d-js-v3.3.zip
+unzip -d $downloads "$downloads/cocos2d-js-v3.3.zip"
+
+# Get Cocos2D-x
+#wget $wget_flags http://www.cocos2d-x.org/filedown/cocos2d-x-3.4.zip
+#unzip -d $downloads "$downloads/cocos2d-x-3.4.zip"
+
+if [ `uname` = Darwin ] ; then
+    # Get various OS X Android parts
+    wget $wget_flags http://dl.google.com/android/android-sdk_r24.1.2-macosx.zip
+    unzip -d $downloads "$downloads/android-sdk_r24.1.2-macosx.zip"
+
+    # Evil, self-extracting installer for Android NDK
+    wget $wget_flags http://dl.google.com/android/ndk/android-ndk-r10d-darwin-x86_64.bin
+    
+    pushd $downloads
+    chmod +x "android-ndk-r10d-darwin-x86_64.bin"
+    ./android-ndk-r10d-darwin-x86_64.bin
+    popd
+
+else
+    # If not running in OS X, assume Linux
+
+    wget $wget_flags http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz
+    tar xzfk "$downloads/android-sdk_r24.1.2-linux.tgz" -C "$downloads"
+
+    # Evil, self-extracting installer for Android NDK
+    wget $wget_flags http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin
+    pushd $downloads
     chmod +x "android-ndk-r10d-linux-x86_64.bin"
     ./android-ndk-r10d-linux-x86_64.bin
-popd
+    popd
     
 fi
 
