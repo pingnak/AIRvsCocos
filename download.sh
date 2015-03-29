9201a235d4b4c7bf497ee28a5d288ac10839eb5d
pingnak <pingnak@gmail.com>
2015-03-29 11:53:17 -0700

    Seemingly downloading
    OS X side downloading prerequisites.

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# For windows users, well, sorry.  I'm not going to write a DOS/cmd batch for you. 
+# For windows users, well, sorry.  I'm not going to write a DOS/cmd batch for you, right now. 
 
 
 # COCOS 2D Requirements
@@ -25,8 +25,57 @@
 # Set BASH options 
 set -o errexit	# Stop running the script if an error occurs
 set -o nounset	# Stop running the script if a variable isn't set
-#set -o verbose	# Echo every command
+set -o verbose	# Echo every command
 
 echo Downloading Pieces
 
+downloads=./dependencies
+wget_flags="--continue --directory-prefix=$downloads"
+
+# Get Apache ANT
+wget $wget_flags https://www.apache.org/dist/ant/binaries/apache-ant-1.9.4-bin.zip
+
+# Get Cocos2D-js
+wget $wget_flags http://www.cocos2d-x.org/filedown/cocos2d-js-v3.3.zip
+
+pushd $downloads
+unzip -u -d ant "apache-ant-1.9.4-bin.zip"
+unzip -u -d cocos2d-js "cocos2d-js-v3.3.zip"
+popd
+
+if [ `uname` = Darwin ] ; then
+    # Adobe AIR SDK
+    wget $wget_flags http://airdownload.adobe.com/air/mac/download/latest/AIRSDK_Compiler.tbz2
+    # OpenFLEX
+#   wget $wget_flags http://www.apache.org/dyn/closer.cgi?path=/flex/installer/3.1/binaries/apache-flex-sdk-installer-3.1.0-bin.dmg
+    # Get various OS X Android parts
+    wget $wget_flags http://dl.google.com/android/android-sdk_r24.1.2-macosx.zip
+    wget $wget_flags http://dl.google.com/android/ndk/android-ndk-r10d-darwin-x86_64.bin
+
+pushd $downloads
+    tar jvxfk -C AIR "AIRSDK_Compiler.tbz2"
+    unzip -u -d android-sdk "android-sdk_r24.1.2-macosx.zip"
+
+    # Evil, self-extracting installer.    
+    chmod +x "android-ndk-r10d-darwin-x86_64.bin"
+    ./android-ndk-r10d-darwin-x86_64.bin
+popd
+        
+else
+    # If not running in OS X, assume Linux
+    wget $wget_flags http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin
+    wget $wget_flags http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz
+    # OpenFLEX
+    wget $wget_flags http://www.apache.org/dyn/closer.cgi?path=/flex/installer/3.1/apache-flex-sdk-installer-3.1.0-src.tar.gz
+
+pushd $downloads
+    tar xvzfk -C flex "apache-flex-sdk-installer-3.1.0-src.tar.gz"
+    tar xvzfk -C 'android-sdk' "android-sdk_r24.1.2-linux.tgz"
+
+    # Evil, self-extracting installer.    
+    chmod +x "android-ndk-r10d-linux-x86_64.bin"
+    ./android-ndk-r10d-linux-x86_64.bin
+popd
+    
+fi
 
---
+++ readme.d
@@ -0,0 +1,12 @@
+# Adobe AIR vs Cocos2d
+
+This implements some build schemes for common middleware, based on recent work 
+with Cocos2D-js and Adobe AIR, specifically.
+
+I decided to share because this is handy stuff, and I wanted a quick way to start into either kind of project.
+
+* I will make 'hands off' project scripts for Cocos2d-js, to keep the source code, art, etc. from being scattered among tens of thousands of intermediate garbage files, and corrects some nasty gotchas, present in version 3.3.
+
+* Share a convenient Apache Ant build script for AIR SDK, which builds all of the supported targets, and deploys/tests/debugs them.
+
+* Document every step, from set up, to submit. This should be useful as an example, for migrating between these environments, and keeping your code safe and portable.  Also, because I have old-people memory.  
